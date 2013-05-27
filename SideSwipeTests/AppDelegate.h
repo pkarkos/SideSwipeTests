9efce6f0d58af6f5323c7e54e6eba5bfff391e5d
Peter Karkos <Peter@67-194-112-149.wireless.umnet.umich.edu>
2013-05-26 23:02:26 -0400

    Initial Commit

---
+++ SideSwipeTests/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  SideSwipeTests
+//
+//  Created by Peter Karkos on 5/26/13.
+//  Copyright (c) 2013 Peter Karkos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ SideSwipeTests/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SideSwipeTests
+//
+//  Created by Peter Karkos on 5/26/13.
+//  Copyright (c) 2013 Peter Karkos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
